156e79923f6ff0c4fda6c689960cb5b15815f24c
cibelesthefany <cibelesthefanysilva@gmail.com>
2023-05-03 17:12:43 -0300

    Cria tela de registro de medicamento

---
+++ app/src/main/java/com/cibesth/medtech/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.cibesth.medtech
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity {
+    class MainActivity : Activity() {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.medtech_medicine_registration)
+        }
+    }
+}
